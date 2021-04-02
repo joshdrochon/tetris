b5178722e6154f386a43c440e291a6b31c7fc06e
Andrew Alseri <adev@Andrews-MBP.lan>
2021-04-02 05:12:33 -0700

    new templates

--- login_register/views.py
+++ login_register/views.py
@@ -3,5 +3,11 @@ from django.shortcuts import render
 def login_view(request):
     return render(request, 'loginandRegister/login_view.html')
 
-def register_view(request):
-    return render(request, 'loginandRegister/register_view.html')
+def dashboard_view(request):
+    return render(request, 'dashboard/dashboard_view.html')
+
+def profile_view(request):
+    return render(request, 'dashboard/profile_view.html')
+
+def player_view(request):
+    return render(request, 'dashboard/player_view.html')
