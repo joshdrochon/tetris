c261e804c3fd5b42229557da3deaac50de9451fe
Rnnie_g <ronaldgibbs20@gmail.com>
2021-04-06 18:14:37 -0500

    accpected changes

--- login_register/urls.py
+++ login_register/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-<<<<<<< HEAD
 from .views import login_view, dashboard_view, profile_view, player_view, login, register, logout
-=======
 from .views import login_view, rankings_view, profile_view, player_view
->>>>>>> new__dev
 
 urlpatterns = [
     path('', login_view),
