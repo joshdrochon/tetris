cee3ab478a549ca5e0b19a109fa14aa036c2e7f8
firoz Syed <syedfiroz2010@gmail.com>
2021-04-07 12:26:30 -0700

    game-reset and Basic Form Validations

--- login_register/models.py
+++ login_register/models.py
@@ -1,14 +1,22 @@
 from django.db import models
+import re
 class UserManager (models.Manager):
-    def register(self, postdata):
-        # pw = bcrypt.hashpw(postdata['password'].encode(), bcrypt.gensalt()).decode()
-        return self.create(
-            first_name = postdata['first_name'],
-            last_name = postdata['last_name'],
-            username = postdata['username'],
-            email = postdata['email'],
-            password = postdata['password'],
-        )
+    def reg_validate(self, postdata):
+        print("i am validating the data now")
+        errors={}
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        PASSWORD_REGEX=re.compile(r'^[a-zA-Z0-9.*%!@^&]')
+        if len(postdata['first_name'])<2:
+            errors['first_name']="First Name should be minimum atleast 2 characters"
+        if len(postdata['last_name'])<2:
+                errors['last_name']="Last Name should be minimum atleast 2 characters"
+        if not EMAIL_REGEX.match(postdata['email']):
+            errors['email']="Invalid Email Address"
+        if len(postdata['password'])< 8:
+            errors['password']="Password should be minimum 8 characters"
+        if postdata['password']!=postdata['confirm_password']:
+            errors['confirm_password']="Passwords should be identical"
+        return errors
 # Create your models here.
 class User(models.Model):
     first_name = models.CharField(max_length=100)
