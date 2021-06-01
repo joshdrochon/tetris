a6cb67f9a9b3d8e8ca269af069273187d66baef4
Josh Rochon <joshrochon@Joshs-MacBook-Pro.local>
2021-06-01 15:41:17 -0700

    add decorator to redirect to login register view if no user is present

--- tetris_app/views.py
+++ tetris_app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from login_register.models import User
+from .decorators import authenticate_user
 
+@authenticate_user
 def game_view(request):
-    user = User.objects.get(id = request.session['user_id'])
+    [user] = User.objects.filter(id = request.session['user_id'])
     context = {
-    'user': user,
-}
+        'user': user,
+    }
     return render(request,'tetrisgame/game_view.html', context)
 
